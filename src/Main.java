56009a24302f6057ea8611502a356f403b537927
Jaydesire <elgovsky@ya.ru>
2022-06-28 13:48:26 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        ThreadGroup threadGroup = new ThreadGroup("threadGroup");
-        Thread[] threads = new Thread[4];
+//        ThreadGroup threadGroup = new ThreadGroup("threadGroup");
+//        Thread[] threads = new Thread[4];
+//
+//        for (int i = 0; i < threads.length; i++) {
+//            threads[i] = new MyThread(threadGroup, ("поток " + (i + 1)));
+//            threads[i].start();
+//        }
+//
+//        try {
+//            Thread.sleep(15_000);
+//        } catch (InterruptedException err) {
+//        }
+//        System.out.println("Завершаю все потоки.");
+//        threadGroup.interrupt();
 
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new MyThread(threadGroup, ("поток " + (i + 1)));
-            threads[i].start();
+        int nThreads = 4;
+
+        MyCallable myCallable = new MyCallable();
+        List<MyCallable> tasks = new ArrayList<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            tasks.add(myCallable);
         }
 
-        try {
-            Thread.sleep(15_000);
-        } catch (InterruptedException err) {
+        System.out.println("invokeAny. Кол-во выведенных в консоль сообщений " + executorService.invokeAny(tasks));
+
+        for (Future<Integer> task :
+                executorService.invokeAll(tasks)) {
+            System.out.println("invokeAll. Кол-во выведенных в консоль сообщений " + task.get());
         }
         System.out.println("Завершаю все потоки.");
-        threadGroup.interrupt();
+        executorService.shutdown();
     }
 }
---
+++ src/MyCallable.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.Callable;
+
+import static java.lang.Thread.currentThread;
+
+public class MyCallable implements Callable<Integer> {
+    int sum = 0;
+    int numberOfMessages = 4;
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            for (int i = 0; i < numberOfMessages; i++) {
+                System.out.println("Я " + currentThread().getName() + ". Всем привет!");
+                sum++;
+                Thread.sleep(2500);
+            }
+        } catch (InterruptedException err) {
+        }
+        return sum;
+    }
+}
